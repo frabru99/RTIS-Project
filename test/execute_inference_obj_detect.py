b01dae74f1f7bd3847dfb8cb34c1ca055780cc0a
SorteFatale <anto.napoli@outlook.com>
2025-07-03 13:10:35 +0200

    Added memcpy_run

--- test/execute_inference_obj_detect.py
+++ test/execute_inference_obj_detect.py
@@ -3,4 +3,4 @@ import time
 
 
 for i in range(30):
-	os.system("python3 inference_obj_detect.py ./photos/ vmrun --network ssd-mobilenet-v2")
+	os.system("python3 inference_obj_detect.py ./photos/ memcpyrun --network ssd-mobilenet-v1")
