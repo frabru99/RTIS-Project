907c03d6daf059814a4f01dc7ca1817b8099a308
SorteFatale <anto.napoli@outlook.com>
2025-07-03 15:06:10 +0200

    Added interrupt_run folder

--- test/execute_inference_img_detect.py
+++ test/execute_inference_img_detect.py
@@ -1,4 +1,4 @@
 import os
 
 for i in range (30):
-	os.system("python3 inference_img_detection.py ./photos/ memcpyrun --network inception-v4")
+	os.system("python3 inference_img_detection.py ./photos/ interruptrun --network inception-v4")
--- test/execute_inference_obj_detect.py
+++ test/execute_inference_obj_detect.py
@@ -3,4 +3,4 @@ import time
 
 
 for i in range(30):
-	os.system("python3 inference_obj_detect.py ./photos/ memcpyrun --network ssd-mobilenet-v1")
+	os.system("python3 inference_obj_detect.py ./photos/ interruptrun --network ssd-mobilenet-v2")
