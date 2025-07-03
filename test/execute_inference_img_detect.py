f27d93c81ef48d4a9016349bb85683adf8d89800
SorteFatale <anto.napoli@outlook.com>
2025-07-03 16:19:51 +0200

    Added open_run folder

--- test/execute_inference_img_detect.py
+++ test/execute_inference_img_detect.py
@@ -1,4 +1,4 @@
 import os
 
 for i in range (30):
-	os.system("python3 inference_img_detection.py ./photos/ interruptrun --network inception-v4")
+	os.system("python3 inference_img_detection.py ./photos/ openrun --network inception-v4")
