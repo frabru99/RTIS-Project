ccdd187f7e4a99f77d1a95258306601f4fac804f
SorteFatale <anto.napoli@outlook.com>
2025-07-04 21:57:07 +0200

    Added some files

--- test/execute_inference_img_detect.py
+++ test/execute_inference_img_detect.py
@@ -1,4 +1,4 @@
 import os
 
 for i in range (30):
-	os.system("python3 inference_img_detection.py ./photos/ cpurun --network googlenet")
+	os.system("python3 inference_img_detection.py ./photos/ udprun --network googlenet")
