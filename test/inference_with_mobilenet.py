33301f2b534e5d1e9e1b6c01ac0e06ef7da0020a
SorteFatale <anto.napoli@outlook.com>
2025-07-04 16:12:55 +0200

    GoogleNet First Experiment

--- test/inference_with_mobilenet.py
+++ test/inference_with_mobilenet.py
@@ -1,55 +1,46 @@
 from transformers import AutoImageProcessor, AutoModelForImageClassification
 from PIL import Image
-import requests
 import torch
 import argparse
 import time
 from time import perf_counter
-
-#Use this script only for mobilenet-v1 and mobilenet-v2
+import os
 
 parser = argparse.ArgumentParser()
-parser.add_argument("filename", type=str, help="filename of the image to process")
+parser.add_argument("filename", type=str, help="folder containing the images to process")
 parser.add_argument("testtype", type=str, help="type of the test executed")
-parser.add_argument("--network", type=str, default="google/mobilenet_v1_1.0_224", help="model to use: mobilenet_v1, mobilenet_v2")
+parser.add_argument("--network", type=str, default="mobilenet_v1", help="model to use: mobilenet_v1, mobilenet_v2")
 args = parser.parse_args()
 
-"""
-MODEL INIZIALIZATION
-"""
-
-#Check if the GPU is available
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
+print("Using device:", device)
 
-print(device)
-
-preprocessor = AutoImageProcessor.from_pretrained("google/"+args.network+"_1.0_224")
-model = AutoModelForImageClassification.from_pretrained("google/"+args.network+"_1.0_224")
-
+model_name = f"google/{args.network}_1.0_224"
+preprocessor = AutoImageProcessor.from_pretrained(model_name)
+model = AutoModelForImageClassification.from_pretrained(model_name)
 model.to(device)
 
-#list of time predictions
 times = []
-for image in os.listdir(args.filename):
-    img = Image.open(image.raw)
+
+for image_file in os.listdir(args.filename):
+    image_path = os.path.join(args.filename, image_file)
+    img = Image.open(image_path).convert("RGB")
 
     inputs = preprocessor(images=img, return_tensors="pt")
+    inputs = {k: v.to(device) for k, v in inputs.items()}
 
     t1 = perf_counter()
     outputs = model(**inputs)
     t2 = perf_counter()
 
-    times.append(t2-t1) #inference time
+    times.append(t2 - t1)
 
     logits = outputs.logits
-
-    # model predicts one of the 1000 ImageNet classes
     predicted_class_idx = logits.argmax(-1).item()
     print("Predicted class:", model.config.id2label[predicted_class_idx])
 
-avg_times = sum(times)/len(times) #process the average time of object detection
-
-print("Average Time of test: ", avg_times)
+avg_time = sum(times) / len(times)
+print("Average Time of test:", avg_time)
 
 with open(args.network + "_result_" + args.testtype + ".txt", "a") as file:
-     file.write(str(avg_times) + "\n")
+    file.write(str(avg_time) + "\n")
