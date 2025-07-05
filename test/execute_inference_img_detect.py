7d7f993db227889b0177730f35050ef8afdc7d20
SorteFatale <anto.napoli@outlook.com>
2025-07-05 16:44:01 +0200

    Added isolation files

--- test/execute_inference_img_detect.py
+++ test/execute_inference_img_detect.py
@@ -1,4 +1,4 @@
 import os
 
 for i in range (30):
-	os.system("python3 inference_img_detection.py ./photos/ udprun --network googlenet")
+	os.system("python3 inference_img_detection.py ./photos/ cpuclocksrun --network inception-v4")
