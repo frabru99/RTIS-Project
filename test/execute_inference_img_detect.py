89d6ea975f5be341c5654b19d7d70994265b28b0
SorteFatale <anto.napoli@outlook.com>
2025-07-07 17:20:20 +0200

    GPU run completed

--- test/execute_inference_img_detect.py
+++ test/execute_inference_img_detect.py
@@ -1,4 +1,4 @@
 import os
 
 for i in range (30):
-	os.system("python3 inference_img_detection.py ./photos/ cpuclocksrun --network inception-v4")
+	os.system("python3 inference_img_detection.py ./photos/ gpurun2400 --network inception-v4")
