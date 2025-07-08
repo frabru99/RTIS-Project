b61e53ad217d20de201a5f33992b776789049bbf
SorteFatale <anto.napoli@outlook.com>
2025-07-08 09:49:57 +0200

    GPU_run completed

--- test/execute_inference_obj_detect.py
+++ test/execute_inference_obj_detect.py
@@ -3,4 +3,4 @@ import time
 
 
 for i in range(30):
-	os.system("python3 inference_obj_detect.py ./photos/ gpurun2400 --network ssd-mobilenet-v1")
+	os.system("python3 inference_obj_detect.py ./photos/ gpurun3200 --network ssd-mobilenet-v1")
